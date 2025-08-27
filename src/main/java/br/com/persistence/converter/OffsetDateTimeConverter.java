570f4de4711ab1818271899753b3b6ad49632e88
Gisela Mariano <gisela-mariano@hotmail.com>
2025-08-27 10:55:53 -0300

    fix(converter): add handling for null value in the toOffsetDateTime method

--- src/main/java/br/com/persistence/converter/OffsetDateTimeConverter.java
+++ src/main/java/br/com/persistence/converter/OffsetDateTimeConverter.java
@@ -7,11 +7,12 @@ import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 
 import static java.time.ZoneOffset.UTC;
+import static java.util.Objects.nonNull;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class OffsetDateTimeConverter {
 
-    public  static OffsetDateTime toOffsetDateTime(final Timestamp value) {
-        return OffsetDateTime.ofInstant(value.toInstant(), UTC);
+    public static OffsetDateTime toOffsetDateTime(final Timestamp value) {
+        return nonNull(value) ? OffsetDateTime.ofInstant(value.toInstant(), UTC) : null;
     }
 }
